5708e8f3ba9d2d8967a17a9ee203f36c7d79fbed
Michelle Mairet <michelle.rk.mairet@gmail.com>
2024-06-12 17:50:27 +0200

    Initial commit SafetyNet Alerts app

---
+++ src/test/java/com/safetynetalerts/safetynet/test/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetalerts.safetynet.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
